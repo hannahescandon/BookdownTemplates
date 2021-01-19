f1fed25fd430c73ae4dc30449b146ecde326bc8c
Phil Escandon <phillip.escandon@gmail.com>
2021-01-18 22:18:28 -0500

    Change Author, theme and format

--- R/my_html_format.R
+++ R/my_html_format.R
@@ -22,7 +22,7 @@ my_html_format = function(toc = TRUE, ...) {
     toc_float = TRUE,
     fig_width = 6.5,
     fig_height = 4,
-    theme = "lumen",
+    theme = "yeti",
     code_folding = "show",
     css = css,
     number_sections = TRUE,
